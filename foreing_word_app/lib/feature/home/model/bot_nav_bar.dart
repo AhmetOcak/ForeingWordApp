8f6708493fdcd5167ef88e5f88e399c0992ca9e4
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-30 21:25:17 +0300

    botnavbar model created

--- foreing_word_app/lib/feature/home/model/bot_nav_bar.dart
+++ foreing_word_app/lib/feature/home/model/bot_nav_bar.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:foreing_word_app/core/constants/constants.dart';
-import 'package:foreing_word_app/feature/home/viewmodel/home_viewmodel.dart';
 
-class BotNavBar extends StatelessWidget {
-  BotNavBar({
-    Key? key, required Function onTap,
+class BotNavBar extends StatefulWidget {
+  const BotNavBar({
+    Key? key,
   }) : super(key: key);
 
-  final int _selectedIndex = 0;
-  late Function onTap;
+  @override
+  State<BotNavBar> createState() => _BotNavBarState();
+}
+
+class _BotNavBarState extends State<BotNavBar> {
+  int _selectedIndex = SELECTED_INDEX;
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +19,25 @@ class BotNavBar extends StatelessWidget {
       backgroundColor: PRIMARY_COLOR,
       selectedItemColor: SECONDARY_COLOR,
       unselectedItemColor: WHITE,
-      elevation: 0,
+      elevation: BOTTOM_NAV_BAR_ELEVATION,
+      currentIndex: _selectedIndex,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
           label: HOME,
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.add),
+          icon: Icon(
+            Icons.add,
+          ),
           label: ADD_WORD,
         ),
       ],
-      onTap: (value) => onTap,
-      currentIndex: _selectedIndex,
+      onTap: (value) {
+        setState(() {
+          _selectedIndex = value;
+        });
+      },
     );
   }
 }
--- foreing_word_app/lib/feature/home/view/home_view.dart
+++ foreing_word_app/lib/feature/home/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foreing_word_app/core/constants/constants.dart';
+import 'package:foreing_word_app/feature/home/model/bot_nav_bar.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -9,7 +10,7 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  int _selectedIndex = 0;
+  int _selectedIndex = SELECTED_INDEX;
 
   @override
   Widget build(BuildContext context) {
@@ -18,36 +19,20 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         title: const Text(
           APP_NAME,
-          style: TextStyle(color: WHITE,),
+          style: TextStyle(
+            color: WHITE,
+          ),
         ),
         backgroundColor: PRIMARY_COLOR,
-        elevation: 0,
+        elevation: APP_BAR_ELEVATION,
       ),
       body: Center(
         child: Column(),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: PRIMARY_COLOR,
-        selectedItemColor: SECONDARY_COLOR,
-        unselectedItemColor: WHITE,
-        elevation: 0,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: HOME,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: ADD_WORD,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: (value) {
-          setState(() {
-            _selectedIndex = value;
-          });
-        },
-      ),
+      bottomNavigationBar: const BotNavBar(),
     );
   }
 }
+
+
+
