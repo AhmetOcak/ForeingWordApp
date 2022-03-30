e3c50fd017c705e884e744c5ff4c19da9510c489
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-30 21:25:08 +0300

    constants added

--- foreing_word_app/lib/core/constants/constants.dart
+++ foreing_word_app/lib/core/constants/constants.dart
@@ -10,4 +10,7 @@ const Color SECONDARY_COLOR = Color(0xFFFFDE03);
 const Color WHITE = Color(0xFFFFFFFF);
 const String HOME = "Home";
 const String ADD_WORD = "Add Word";
+const double APP_BAR_ELEVATION = 0;
+const double BOTTOM_NAV_BAR_ELEVATION = 0;
+const int SELECTED_INDEX = 0;
 
