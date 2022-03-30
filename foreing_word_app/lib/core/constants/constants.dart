5489bb85b711a5aec8be0a1cf1d0b45de4008964
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-30 21:15:14 +0300

    constants added

---
+++ foreing_word_app/lib/core/constants/constants.dart
@@ -0,0 +1,13 @@
+
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/cupertino.dart';
+
+const String APP_NAME = "ForeingWord";
+
+const Color PRIMARY_COLOR = Color(0xFF0336FF);
+const Color SECONDARY_COLOR = Color(0xFFFFDE03);
+const Color WHITE = Color(0xFFFFFFFF);
+const String HOME = "Home";
+const String ADD_WORD = "Add Word";
+
