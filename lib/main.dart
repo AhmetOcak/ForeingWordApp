6c076589693a3fa8fba250aeb186af69c61a80a5
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-31 14:26:07 +0300

    shared preferences package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:foreing_word_app/core/utils/user_simple_preferences.dart';
 import 'package:foreing_word_app/feature/main_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await UserSimplePreferences.init();
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: const MainPage(),
