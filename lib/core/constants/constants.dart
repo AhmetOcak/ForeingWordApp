37a914c29536bad4168084cf0a355c4a48c3e72b
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-31 12:54:51 +0300

    constants added

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: constant_identifier_names
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
 
 import 'package:flutter/cupertino.dart';
 import 'package:foreing_word_app/feature/add_word/view/addword_view.dart';
@@ -11,6 +11,9 @@ const Color SECONDARY_COLOR = Color(0xFFFFDE03);
 const Color WHITE = Color(0xFFFFFFFF);
 const String HOME = "Home";
 const String ADD_WORD = "Add Word";
+const String BUTTON_TEXT = "Add the Word";
+const String TEXTFIELD_P_TEXT = "Primary Word";
+const String TEXTFIELD_S_TEXT = "Secondary Word";
 const int SELECTED_INDEX = 0;
 const double ELEVATION = 0;
 const double CARD_HEIGHT = 150;
@@ -19,13 +22,15 @@ const double CARD_VERTICAL_PADDING = 10;
 const Color CARD_TEXT_COLOR = Color(0xFF000000);
 const double CARD_TEXT_SIZE = 20;
 const double DEFAULT_APP_BAR_HEIGHT = 56.0;
+const double TEXTFIELD_TB_PADDING = 16.0;
+const double BUTTON_TOP_PADDING = 16.0;
+const double BUTTON_TEXT_PADDING = 16.0;
+const double BUTTON_TEXT_SIZE = 16.0;
 
 const PAGES = [
   HomeView(),
   AddWordView(),
 ];
-
-int selectedIndex = 0;
 
 TextStyle cardTextStyle() {
   return const TextStyle(
