e9d9862a8c23c76787f8839e61921cc8007effe5
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-31 13:30:19 +0300

    class name changed

--- lib/feature/add_word/model/textfied.dart
+++ lib/feature/add_word/model/textfied.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:foreing_word_app/core/components/textfield/custom_textfield.dart';
 import 'package:foreing_word_app/core/constants/constants.dart';
 
-class Model {
-  CustomTextField customTextField(BuildContext context, TextEditingController textEditingController) {
+class TextFieldModel {
+  CustomTextField customTextField(
+      BuildContext context, TextEditingController textEditingController) {
     return CustomTextField(
       controller: textEditingController,
       padding: EdgeInsets.fromLTRB(
