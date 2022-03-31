a15c95fc87e970fb4876a564b8dee7c4adca0e0b
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-31 17:13:18 +0300

    listviewbuilder added

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:foreing_word_app/core/components/appbar/custom_appbar.dart';
 import 'package:foreing_word_app/core/constants/constants.dart';
+import 'package:foreing_word_app/core/utils/user_simple_preferences.dart';
 import 'package:foreing_word_app/feature/home/model/word_card.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  List<String> _wordList = [];
+
+  @override
+  void initState() {
+    _wordList = UserSimplePreferences.getWordList() ?? [];
+    super.initState();
+    print(_wordList);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +35,14 @@ class HomeView extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView(
-        children: [
-          WordCard(
-            primaryWord: "alien",
-            secondaryWord: "uzaylı",
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: _wordList.length ~/ 2,
+        itemBuilder: (context, index) {
+          return WordCard(
+            primaryWord: _wordList[index * 2],
+            secondaryWord: _wordList[(index * 2) + 1],
+          );
+        },
       ),
     );
   }
