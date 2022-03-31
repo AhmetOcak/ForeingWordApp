3dc88231354a9bcef0b22b243fb3848490da1053
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-31 17:52:53 +0300

    delete func added

--- lib/core/utils/user_simple_preferences.dart
+++ lib/core/utils/user_simple_preferences.dart
@@ -16,6 +16,12 @@ class UserSimplePreferences {
     _preferences.setStringList(_key, _wordList!);
   }
 
+  static Future deleteWordFromList(int primaryWordIndex, int secondaryWordIndex) async {
+    _wordList!.removeAt(primaryWordIndex);
+    _wordList!.removeAt(secondaryWordIndex - 1);
+    _preferences.setStringList(_key, _wordList!);
+  }
+
   static List<String>? getWordList() => _preferences.getStringList(_key);
 
 }
