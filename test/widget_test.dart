db908e899de1b9e9536656bdd83da14dfcb2337e
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-04-08 23:45:20 +0300

    problem solved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:foreing_word_app/feature/main_page.dart';
 
 import 'package:foreing_word_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MainPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
