2dc55ab75a92a212bde9e71aeca76e35d6bf22f6
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-06 14:53:47 +0300

    action added

--- lib/feature/home/model/app_bar.dart
+++ lib/feature/home/model/app_bar.dart
@@ -3,12 +3,22 @@ import 'package:foreing_word_app/core/components/appbar/custom_appbar.dart';
 import 'package:foreing_word_app/core/constants/constants.dart';
 
 class AppBarModel {
-  CustomAppBar customAppBar() {
-    return const CustomAppBar(
+  CustomAppBar customAppBar(BuildContext context) {
+    return CustomAppBar(
       elevation: ELEVATION,
-      title: Text(
+      title: const Text(
         APP_NAME,
       ),
+      action: [
+        IconButton(
+          onPressed: () {
+            Navigator.pushNamed(context, WORD_GAME_SCREEN);
+          },
+          icon: const Icon(
+            Icons.gamepad,
+          ),
+        ),
+      ],
     );
   }
 }
